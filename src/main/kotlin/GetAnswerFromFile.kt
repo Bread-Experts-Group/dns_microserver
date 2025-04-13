cb76c992e2e80dd58285d598fdc74abfd91c693b
ATPStorages <tcgytsc@gmail.com>
2025-04-13 10:55:17 -0500

    HINFO/CNAME/HTTPS support (HTTPS in progress)

--- src/main/kotlin/GetAnswerFromFile.kt
+++ src/main/kotlin/GetAnswerFromFile.kt
@@ -3,6 +3,7 @@ package bread_experts_group
 import bread_experts_group.dns.DNSClass
 import bread_experts_group.dns.DNSResourceRecord
 import bread_experts_group.dns.DNSType
+import bread_experts_group.dns.https.HTTPSParameters
 import bread_experts_group.dns.writeLabel
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -29,11 +30,49 @@ fun getAnswerFromFile(name: String, file: File): DNSResourceRecord {
 			it.toByteArray()
 		}
 
+		"HTTPS" -> ByteArrayOutputStream().use {
+			it.write16(stream.scanDelimiter("\n").toInt())
+			it.write(writeLabel(stream.scanDelimiter("\n")))
+			while (stream.available() == 0) {
+				val parameter = HTTPSParameters.valueOf(stream.scanDelimiter("\n"))
+				it.write16(parameter.code)
+				when (parameter) {
+					HTTPSParameters.MANDATORY -> {
+						val mandatory = stream.scanDelimiter("\n").split(',')
+						it.write16(mandatory.size * 2)
+						mandatory.forEach { key -> it.write16(HTTPSParameters.valueOf(key).code) }
+					}
+
+					HTTPSParameters.ADDITIONAL_SUPPORTED_PROTOCOLS -> {
+						val alpns = stream.scanDelimiter("\n").split(',')
+						it.write16(alpns.sumOf { it.length } + alpns.size)
+						alpns.forEach { alpn ->
+							it.write(alpn.length)
+							it.writeString(alpn)
+						}
+					}
+
+					else -> throw UnsupportedOperationException("Unsupported HTTP parameter: $parameter")
+				}
+			}
+			it.toByteArray()
+		}
+
+		"HINFO" -> ByteArrayOutputStream().use {
+			val cpu = stream.scanDelimiter("\n")
+			it.write(cpu.length)
+			it.writeString(cpu)
+			val remainder = stream.readAllBytes().decodeToString().trim()
+			it.write(remainder.length)
+			it.writeString(remainder)
+			it.toByteArray()
+		}
+
 		else -> {
 			val remainder = stream.readAllBytes().decodeToString().trim()
 			when (file.extension) {
 				"A" -> Inet4Address.getByName(remainder).address
-				"NS", "PTR" -> writeLabel(remainder)
+				"NS", "PTR", "CNAME" -> writeLabel(remainder)
 				"TXT" -> ByteArrayOutputStream().use {
 					it.write(remainder.length)
 					it.writeString(remainder)
