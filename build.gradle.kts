732bbce67762f1fdbaa9a8daad5a0c283ed872d5
ATPStorages <tcgytsc@gmail.com>
2025-04-15 13:20:08 -0500

    BSL Bugfix

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "bread_experts_group"
-version = "1.8"
+version = "1.8.1"
 
 repositories {
 	mavenCentral()
@@ -20,7 +20,7 @@ repositories {
 dependencies {
 	testImplementation(kotlin("test"))
 	implementation(kotlin("reflect"))
-	implementation("bread_experts_group:bread_server_lib-code:1.17")
+	implementation("bread_experts_group:bread_server_lib-code:1.17.1")
 }
 
 tasks.test {
