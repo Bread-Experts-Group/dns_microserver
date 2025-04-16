e881aa09db58c6d695c97ea9fcca0db7d1a74e71
ATPStorages <tcgytsc@gmail.com>
2025-04-16 11:28:57 -0500

    UDP truncation move bit position.

--- src/main/kotlin/DNSMain.kt
+++ src/main/kotlin/DNSMain.kt
@@ -45,7 +45,7 @@ fun main(args: Array<String>) {
 				val localLogger = Logger.getLogger("DNS UDP ${packet.socketAddress}")
 				val reply = dnsExecution(localLogger, recordStore, packet.data)
 				if (reply != null) {
-					if (reply.size > 512) reply[2] = (reply[2].toInt() or 0b10).toByte()
+					if (reply.size > 512) reply[2] = (reply[2].toInt() or 0b01000000).toByte()
 					packet.setData(reply, 0, 512)
 					udpSocket.send(packet)
 				}
